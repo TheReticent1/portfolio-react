ca1973563c4e226a4f96d766075c96b267c3f441
TheReticent1 <kamleshmohane78@gmail.com>
2025-02-04 22:28:05 +0530

    popup skills in progress

--- src/components/about-me/about-me.jsx
+++ src/components/about-me/about-me.jsx
@@ -2,17 +2,49 @@ import { useState } from 'react';
 import Popup from '../../shared/components/popup/popup';
 import './about-me.css';
 const timelineData = [
-    { year:"2025 - Persent", heading:"Job Hunting", description:"Looking for React js job at entry level in Pune."},
-    { year:"2024", heading:"Internship", description:"Pursued internship in Full stack development using react and java"},
-    { year:"2021 - 2024", heading:"Engineering", description:"Pursued engineering degree in Electronics and telecommunication"},
-    { year:"2018 - 2021", heading:"Diploma", description:"Pursued diploma in Information technology"},
-    { year:"2014 - 2016", heading:"HSC", description:"Science student"},
-    { year:"2013-2014", heading:"SSC", description:"Passout from school"},
+    {   year:"2025 - Persent", 
+        heading:"Job Hunting", 
+        description:"Looking for React js job at entry level in Pune.",
+        popupDescription: "Seeking an entry-level React.js developer position in Pune where I can utilize my skills in JavaScript and React.js to contribute to dynamic projects, continue learning, and grow as part of a collaborative team.",
+        skills: [
+            {skillName:"Javascript", rating:7},
+            {skillName:"HTML5", rating:7},
+            {skillName:"CSS", rating:7},
+            {skillName:"React.js", rating:5},
+            {skillName:"GIT", rating:6},
+            {skillName:"MySql", rating:7},
+        ]
+    },
+    { year:"2024", heading:"Internship", description:"Pursued internship in Full stack development using react and java",
+        popupDescription: "I am a passionate Full Stack Developer with hands-on experience from my internship, where I worked with React.js and Java to build dynamic, user-friendly applications. I gained valuable skills in both front-end and back-end development, focusing on creating seamless, responsive interfaces and efficient server-side solutions. Eager to apply my technical expertise and continue growing in the development field.",
+        skills: [],
+    },
+    { year:"2021 - 2024", heading:"Engineering", description:"Pursued engineering degree in Electronics and telecommunication",
+        popupDescription:"I have completed my Engineering degree in Electronics and Telecommunication with a CGPA of 7.8. During my studies, I gained a strong foundation in both hardware and software concepts, with a particular interest in programming and technology. This background has equipped me with problem-solving skills and a technical mindset, which I now apply to my career in full-stack development.",
+        skills: [],
+
+    },
+    { year:"2018 - 2021", heading:"Diploma", description:"Pursued diploma in Information technology", 
+        popupDescription:"I have completed a Diploma in Information Technology, where I developed a solid understanding of computer science fundamentals, programming, and system design. This academic experience provided me with a strong technical foundation, which I continue to build upon in my career as a Full Stack Developer, focusing on both front-end and back-end technologies.",
+        skills: [],
+
+    },
+    { year:"2014 - 2016", heading:"HSC", description:"Science student", 
+        popupDescription:'',
+        skills: [],
+    },
+    { year:"2013-2014", heading:"SSC", description:"Passout from school",
+        popupDescription:'',
+        skills: [],
+    },
 ]
 const AboutMe = () => {
     const [visible, setVisible] = useState(false);
-    const handleClick = (val) => {
+    const [cardData, setCardData] = useState(null);
+    const handleClick = (val, cardDetails) => {
         setVisible(val);
+        setCardData(cardDetails);
+        document.body.style.overflowY = val ? 'hidden' : 'auto';
     }
     return<>
         <div className="container">
@@ -28,7 +60,7 @@ const AboutMe = () => {
                             {
                                 timelineData.map((card, index) => {
                                     return <li key={index}>
-                                            <div className="timeline-card" onClick={() => handleClick(true)}>
+                                            <div className="timeline-card" onClick={() => handleClick(true, card)}>
                                                 <p>{card.year}</p>
                                                 <h3>{card.heading}</h3>
                                                 <p>{card.description}</p>
@@ -41,7 +73,7 @@ const AboutMe = () => {
                 </div>
             </div>
         </div>
-        <Popup visible={visible} onHandleClick={handleClick}/>
+        <Popup visible={visible} onHandleClick={handleClick} popupData={cardData}/>
     </>
 }
 
--- src/shared/components/popup/popup.jsx
+++ src/shared/components/popup/popup.jsx
@@ -1,21 +1,28 @@
 import './popup.css'
-const Popup = ({visible, onHandleClick}) => {
+const Popup = ({visible, onHandleClick, popupData}) => {
     //early check 
     if(!visible) return null;
     return <>
         <div className="popup-backdrop">
             <div className="popup-content">
                 <div className="popup-header">
-                    <div className='popup-heading'>Heading <span> | 2025</span></div>
+                    <div className='popup-heading'>{popupData.heading} <span> | {popupData.year}</span></div>
                     <div>
-                        <button onClick={() => onHandleClick(false)}>X</button>
+                        <button onClick={() => onHandleClick(false, null)}>X</button>
                     </div>
                 </div>
                 <div className="popup-body">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                        Odio deserunt a iure sapiente ducimus! Dignissimos dicta blanditiis sunt dolorum possimus deserunt 
-                        praesentium voluptate cum temporibus, doloremque sequi maiores nesciunt provident?
-                    </p>
+                    <div>
+                        <p>{popupData.popupDescription}</p>
+                    </div>
+                    <div className='popup-skills'>
+                        {popupData.skills.length ? <h3 className='h3'>What I know.</h3>:<span></span>}
+                        {
+                            popupData.skills.length ? (popupData.skills.map((skill, index) => {
+                                return <button key={`skill_id_${index}`}>{skill.skillName}</button>
+                            })): <span></span>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
